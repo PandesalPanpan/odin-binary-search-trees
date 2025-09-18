4fb184d2763e0de59aadf79deac5da6d1671f0f7
PandesalPanpan <petermarticio@gmail.com>
2025-09-18 08:25:04 +0800

    Clean: Formatting isSortedUnique method

--- tree.js
+++ tree.js
@@ -79,7 +79,7 @@ export default class Tree {
     */
     isSortedUnique = (array) => {
         for (let i = 0; i < array.length - 1; i++) {
-            if (array[i] >= array[i+1]) {
+            if (array[i] >= array[i + 1]) {
                 return false;
             } 
         }
